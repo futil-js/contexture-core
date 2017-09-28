ba303b97909bc6054086d270df7be0e290638b64
Samuel Greene <sgreene@smartprocure.us>
2017-09-29 00:08:50 +0200

    add example debug provider

---
+++ src/provider-debug/index.js
@@ -0,0 +1,24 @@
+let _ = require('lodash/fp')
+
+let DebugProvider = {
+  groupCombinator: (group, filters) => ({
+    [group.join]: filters
+  }),
+  runSearch: function(context, schema, filters, aggs) {
+    let request = _.defaults({}, filters, aggs)
+    context._meta.requests.push(request)
+    return Promise.resolve(request)
+  },
+  types: {
+    default: {
+      hasValue: () => true,
+      filter: _.get('key'),
+      result: (context, search) =>
+        search(null).return({
+          abc: 123
+        })
+    }
+  }
+}
+
+module.exports = DebugProvider
