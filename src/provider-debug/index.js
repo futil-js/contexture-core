97a84882e68ffe3a7f59201463e18705f32b4aed
Samuel Greene <sgreene@smartprocure.us>
2017-09-29 21:43:28 +0200

    improve debug provider

--- src/provider-debug/index.js
+++ src/provider-debug/index.js
@@ -1,23 +1,33 @@
 let _ = require('lodash/fp')
+let Promise = require('bluebird')
 
 let DebugProvider = {
   groupCombinator: (group, filters) => ({
     [group.join]: filters,
   }),
-  runSearch: function(context, schema, filters, aggs) {
-    let request = _.defaults({}, filters, aggs)
+  runSearch: function(options, context, schema, filters, aggs) {
+    let request = {where: filters, retrieve: aggs}
     context._meta.requests.push(request)
     return Promise.resolve(request)
   },
   types: {
     default: {
-      hasValue: () => true,
-      filter: _.get('key'),
+      validContext: () => true,
+      hasValue: () => true
+    },
+    test: {
+      filter: x => ({[`${x.field || x.key} (${x.type})`]: x.data}),
       result: (context, search) =>
         search(null).return({
           abc: 123,
         }),
     },
+    results: {
+      result: (context, search) =>
+        search({ results: context.config }).return({
+          results: []
+        })
+    }
   },
 }
 
