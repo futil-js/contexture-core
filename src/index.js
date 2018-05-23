bc264d887707e257f51fb032efd1423a33c4932c
Samuel Greene <sgreene@smartprocure.us>
2018-05-23 01:26:39 +0000

    refactoring - factor out walk/overAsync, always pass schema and processorConfig in runProcessor, rename processStep to walk

--- src/index.js
+++ src/index.js
@@ -51,29 +51,37 @@ let runTypeProcessor = _.curry(
   }
 )
 
+let overAsync = fns => _.flow(_.over(fns), Promise.all)
+let walkAsync = tree => f => parentFirstDFS(getChildren, f, tree)
 let process = _.curryN(
   2,
   async ({ providers, schemas }, groupParam, options = {}) => {
+    let processGroup = g => process({providers, schemas }, g, options)
     let getProvider = utils.getProvider(providers, schemas)
-    let runProcessor = runTypeProcessor(getProvider)
     let getSchema = schema => schemas[schema]
+    let processorConfig = { getProvider, getSchema, options, processGroup }
+    let runProcessor = (...args) => {
+      let schema = getSchema(args[1])
+      return runTypeProcessor(getProvider, ...args, schema, processorConfig)
+    }
     let group = _.cloneDeep(groupParam)
-    let processStep = f => parentFirstDFS(getChildren, f, group)
+    let walk = walkAsync(group)
     try {
-      await processStep(_.flow(_.over([
-        makeObjectsSafe,
-        flattenLegacyFields,
-        materializePaths,
-        async item => {
-          let schema = getSchema(item.schema)
-          let hasValue = await runProcessor('hasValue', item, schema)
-          item._meta.hasValue = hasValue
-          if (hasValue && !item.contextOnly) {
-            item._meta.filter = await runProcessor('filter', item, schema)
-          }
-        },
-      ]), Promise.all))
-      await processStep(item => {
+      await walk(
+        overAsync([ // Do all of these in the same traversal
+          makeObjectsSafe,
+          flattenLegacyFields,
+          materializePaths,
+          async item => {
+            let hasValue = await runProcessor('hasValue', item)
+            item._meta.hasValue = hasValue
+            if (hasValue && !item.contextOnly) {
+              item._meta.filter = await runProcessor('filter', item)
+            }
+          },
+        ])
+      )
+      await walk(item => {
         // Skip groups
         if (!getChildren(item))
           item._meta.relevantFilters = getRelevantFilters(
@@ -82,9 +90,9 @@ let process = _.curryN(
             group
           )
       })
-      await processStep(async item => {
+      await walk(async item => {
         let schema = getSchema(item.schema)
-        let validContext = await runProcessor('validContext', item, schema)
+        let validContext = await runProcessor('validContext', item)
 
         // Reject filterOnly
         if (item.filterOnly || !validContext) return
@@ -96,22 +104,13 @@ let process = _.curryN(
           item._meta.relevantFilters,
         ])
 
-        let result = await runProcessor(
-          'result',
-          item,
-          curriedSearch,
-          schema,
-          { getProvider, getSchema },
-          options
-        ).catch(error => {
-          throw F.extendOn(error, {
-            item,
-          })
-        })
+        let result = await runProcessor('result', item, curriedSearch).catch(
+          error => { throw F.extendOn(error, { item }) }
+        )
         item.context = result
         if (options.onResult) options.onResult(result)
       })
-      await processStep(item => {
+      await walk(item => {
         if (!options.debug) delete item._meta
       })
 
